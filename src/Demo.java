0315112e5705f0f4e0ddbf1ef23dee4926ff7bd5
rmaddirala <rmaddirala@US>
2024-07-31 01:51:30 +0530

    first code

---
+++ src/Demo.java
@@ -0,0 +1,14 @@
+
+public class Demo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("hhjhkjs");
+		System.out.print("hhjhkjs");
+		System.out.println("hhjhkjs");
+
+
+
+	}
+
+}
